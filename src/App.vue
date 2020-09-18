1642a0b20ea373eec3e51965bba8846b9e29bab6
Ivan <ilushinvanya@gmail.com>
2020-09-19 01:21:10 +0300

    работа с различными типами списка значений

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,73 @@
 <template lang="pug">
-#app
-	Select(:placeholder="'Сделай свой выбор'")
-		template(v-slot:placeholder)
-			span Custom placeholder
+	#app
+		Select(
+			v-model="select"
+			:placeholder="'Сделай свой выбор'"
+			:options="options_5"
+			:disabled="false"
+			:label_field="'title'"
+			:value_field="'id'"
+		)
+			template(v-slot:placeholder)
+				span Custom placeholder
 
+		p {{ select }}
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Select from './components/Select.vue';
+    import {Component, Vue} from 'vue-property-decorator';
+    import Select from './components/Select.vue';
 
-@Component({
-  components: {
-    Select
-  }
-})
-export default class App extends Vue {}
+    @Component({
+        components: {
+            Select
+        },
+        data() {
+            return {
+                select: false,
+                options_1: ['Option1', 'Option2', 'Option3'],
+				options_2: [
+                    {
+                        label: 'Preetish',
+                        value: 1
+                    },
+                    {
+                        label: 'John',
+                        value: 2
+                    },
+                    {
+                        label: 'Barbara',
+                        value: 3
+                    },
+                    {
+                        label: 'Gena',
+                        value: 4
+                    }
+                ],
+				options_3: [
+				    { name: 'Option 1', id: 1},
+					{ name: 'Option 2', id: 2 }
+				],
+				options_4: {
+                    value1: 'Option 1',
+					value2: 'Option 2'
+				},
+				options_5: {
+                    value1: { title: 'Option 1', id: 1 },
+					value2: { title: 'Option 2', id: 2 }
+                }
+
+
+        }
+        }
+    })
+    export default class App extends Vue {}
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  color #2c3e50
+	#app
+		font-family Avenir, Helvetica, Arial, sans-serif
+		-webkit-font-smoothing antialiased
+		-moz-osx-font-smoothing grayscale
+		color #2c3e50
 </style>
--- src/components/Select.vue
+++ src/components/Select.vue
@@ -6,22 +6,23 @@
 				@click="toogleOpen"
 				:class="{disabled}")
 
-				.select_value {{input}}
-				.select_placeholder(v-if="!input")
+				.select_value {{value_label}}
+
+				.select_placeholder(v-if="!value")
 					slot(name="placeholder") {{placeholder}}
 
 			.select_clear(
-				v-if="input"
+				v-if="value"
 				@click="clearInput")
 
 		.select_options(:class="{hidden: !open, visible: open}")
 			.option(
-				v-for="option in list"
-				@click="changeOption(option.label)"
+				v-for="option in options_parse"
+				@click="changeOption(option.value)"
 				@mouseover="focus_option = option.value"
 				@mouseleave="focus_option = 0"
 				:class="{focus: focus_option === option.value}"
-				) {{option.label}}
+			) {{option.label}}
 </template>
 
 <script lang="ts">
@@ -30,42 +31,90 @@
     @Component
     export default class Select extends Vue {
         @Prop() private placeholder!: string;
-        private input: string = "input";
-        private focus_option: number = 0;
-        private disabled: boolean = false;
-        private open: boolean = false;
-        private list: Array<object> = [
-            {
-                label: 'Preetish',
-                value: 1
-            },
-            {
-                label: 'John',
-                value: 2
-            },
-            {
-                label: 'Barbara',
-                value: 3
-            },
-            {
-                label: 'Gena',
-                value: 4
-            }
-        ]
+        @Prop() private options!: Array<any>;
+        @Prop() private value!: any;
+        @Prop({default: false}) private disabled!: boolean;
+        @Prop({default: 'label'}) readonly label_field!: string;
+        @Prop({default: 'value'}) readonly value_field!: string;
 
-        public clearInput(): void {
-            if (this.disabled) return;
-            this.input = ""
-		}
-        public changeOption(option_name: string): void {
-            this.input = option_name;
-            // this.$emit("input", option)
+        private focus_option: number = 0;
+        private open: boolean = false;
+
+        private clearInput(): void {
+            if (this.disabled) return
+            this.open = false
+            this.$emit("input", false)
+        }
+
+        private changeOption(option_value: any): void {
+            this.$emit("input", option_value);
             this.toogleOpen()
         }
 
-        public toogleOpen(): void {
+        private toogleOpen(): void {
             if (this.disabled) return;
             this.open = !this.open;
+        }
+
+        get value_label(): string {
+            const find_of_option = this.options_parse.filter(option => option.value === this.value);
+
+            if ( find_of_option !== undefined){
+            	if ( Array.isArray(find_of_option) && find_of_option.length > 0){
+					return find_of_option[0].label;
+				}
+			}
+            return ""
+        }
+
+        get options_parse(): Array<object> {
+            let result_options = [];
+
+            const optionsIsArray = this.options instanceof Array;
+
+            if ( typeof this.options === 'object' && !optionsIsArray ) {
+                for (const [key, value] of Object.entries(this.options)) {
+
+                    if ( typeof value === 'object' ){
+
+                        result_options.push({
+                            label: value[this.label_field],
+                            value: value[this.value_field]
+                        })
+
+                    }else{
+
+                        result_options.push({
+                            label: value,
+                            value: key
+                        })
+
+                    }
+                }
+            }
+
+            if ( optionsIsArray ) {
+
+                this.options.forEach(option => {
+                    if (typeof option === 'string') {
+
+                        result_options.push({
+                            label: option,
+                            value: option
+                        })
+
+                    } else if (typeof option === 'object') {
+
+                        result_options.push({
+                            label: option[this.label_field],
+                            value: option[this.value_field]
+                        })
+
+                    }
+                })
+            }
+
+            return result_options;
         }
 
 
@@ -82,8 +131,10 @@
 			border 1px blue solid
 			border-radius 5px
 			overflow hidden
+
 			&_input
 				padding 10px
+
 				&.disabled:before
 					content ""
 					position absolute
@@ -93,15 +144,34 @@
 					top 0
 					left 0
 					background rgba(255, 255, 255, 0.7)
+
 		&_clear
 			position absolute
 			right 7px
 			top 50%
 			transform translateY(-50%)
-			background #adadad
 			width 18px
 			height 18px
 			cursor pointer
+			opacity: 0.3;
+
+			&:hover
+				opacity 1
+
+			&:before, &:after
+				position absolute
+				left 8px
+				content ''
+				height 100%
+				width 2px
+				background-color #333
+
+			&:before
+				transform rotate(45deg)
+
+			&:after
+				transform rotate(-45deg)
+
 		&_options
 			border 1px #c1c1c1 solid
 			border-bottom 0
@@ -115,6 +185,7 @@
 			.option
 				padding 10px
 				border-bottom 1px #c1c1c1 solid
+
 				&.focus
 					background #dddae2
 </style>
